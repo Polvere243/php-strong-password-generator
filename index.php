4337138b8acac45b24b8a0b4950dfe680ff8749c
Polvere243 <danielercoli@gmail.com>
2024-02-22 01:57:35 +0100

    started thinking about the function, but have an error

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php 
+
+$password_length = $_GET['password-length'];
+
+function generatePassword(length) {
+    $numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    $letters = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
+
+    $letters_numbers = [...$numbers, ...$letters];
+
+    
+    
+}
+
+generatePassword('password-length');
+
 ?>
 
 <!DOCTYPE html>
@@ -10,7 +25,7 @@
 </head>
 <body>
     <form action="" method="get">
-        <input type="number" min="1">
+        <input type="number" name="password-length" min="1">
         <input type="submit" value="Invia" >
     </form>
 </body>
