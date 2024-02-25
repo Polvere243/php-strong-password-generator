1a0090e7d280194bda9da685be4277a1acc3d884
Polvere243 <danielercoli@gmail.com>
2024-02-25 11:02:23 +0100

    added a comment

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ function generatePassword ($password_length)
 
     // creo una variabile password vuota
     $password = '';
-
+    // stabilisco i caratteri 
     $numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
     $letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     $symbols = ['!', '?', '£', '%', '/', '=', '§', '{', '}', '[', ']', '$', '*']; 
