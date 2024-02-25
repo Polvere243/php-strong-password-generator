b459a3543f50547475073823ea6af8298629507f
Polvere243 <danielercoli@gmail.com>
2024-02-25 11:05:42 +0100

    created a variable to calculate the total of characters

--- index.php
+++ index.php
@@ -11,7 +11,10 @@ function generatePassword ($password_length)
     $numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
     $letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     $symbols = ['!', '?', '£', '%', '/', '=', '§', '{', '}', '[', ']', '$', '*']; 
+    // spargo i caratteri in un array
     $characters = [...$numbers, ...$letters, ...$symbols];
+    // calcolo quanti caratteri sono presenti nell'array
+    $total_characters = count($characters);
     while (mb_strlen($new_password)< $password_length && str_contains($new_password, $character));
     $new_password = '';
     $text = explode('', $new_password);
